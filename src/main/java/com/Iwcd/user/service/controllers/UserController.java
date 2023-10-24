391e798d84a73a4f1b8041fff57528294260f0f8
Shaikhismail087 <shaikhismail0876262@gmail.com>
2023-10-24 19:36:23 +0530

    resilience4j configuration in YML(Closed=server working,Open=server down,Half Open)states of Fault Tolerance

--- src/main/java/com/Iwcd/user/service/controllers/UserController.java
+++ src/main/java/com/Iwcd/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.Iwcd.user.service.controllers;
 
 import com.Iwcd.user.service.entities.User;
 import com.Iwcd.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger= LoggerFactory.getLogger(UserController.class);
+
     //create
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
@@ -24,9 +29,23 @@ public class UserController {
 
     //singleUser get
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        logger.info("Get Single User handler: UserController");
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //creating fallback method for circuitBreaker
+    public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+        logger.info("Fallback is executed because service is down : ",ex.getMessage());
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user created dummy because some service is down")
+                .userId("141234")
+                .build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     //all user get
