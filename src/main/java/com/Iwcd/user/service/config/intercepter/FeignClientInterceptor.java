7bbf974d8779758144dc86ece86ba2c3f98e8b76
Shaikhismail087 <shaikhismail0876262@gmail.com>
2023-10-30 00:47:20 +0530

    Feignclient Interceptor & RestTemplate Interceptor

--- src/main/java/com/Iwcd/user/service/config/intercepter/FeignClientInterceptor.java
+++ src/main/java/com/Iwcd/user/service/config/intercepter/FeignClientInterceptor.java
@@ -2,6 +2,7 @@ package com.Iwcd.user.service.config.intercepter;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class FeignClientInterceptor implements RequestInterceptor {
 
+    @Autowired
     private OAuth2AuthorizedClientManager manager;
 
     @Override
